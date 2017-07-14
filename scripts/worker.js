ebfb9a1b569282fe2732dfc4c19f3ca5b352dc8c
Chin Hsu <dblsesame@gmail.com>
2017-07-14 18:11:52 -0400

    move the function determination outside of the loop

--- scripts/worker.js
+++ scripts/worker.js
@@ -3,7 +3,8 @@ importScripts('imageManips.js');
 this.onmessage = function(e) {
   var imageData = e.data.imageData;
   var type = e.data.type;
-
+  //get the function based on type outside of the loop
+  var manipulatePixel = getFunctionByType(type);
   try {
     length = imageData.data.length / 4;
     for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
@@ -11,7 +12,7 @@ this.onmessage = function(e) {
       g = imageData.data[i * 4 + 1];
       b = imageData.data[i * 4 + 2];
       a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
+      pixel = manipulatePixel(r, g, b, a);
       imageData.data[i * 4 + 0] = pixel[0];
       imageData.data[i * 4 + 1] = pixel[1];
       imageData.data[i * 4 + 2] = pixel[2];
